516077dc0935f9825d3a7424e612a4053dff9855
miv3r <miver@miver.me>
2017-11-30 18:30:04 +0300

    Added exceptions

--- gnib_checker.py
+++ gnib_checker.py
@@ -6,14 +6,16 @@ import json
 import requests
 import telebot
 
-URL = 'https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?openpage&cat=Work&sbcat=All&typ=Renewal'
-REQUEST_URL = '{url}&_={time}'
+# configuration
 START_DATE = datetime(2017, 10, 02, 00, 00, 00)
 END_DATE = datetime(2017, 12, 5, 23, 59, 59)
-SLOT_FORMAT = '%d %B %Y - %H:%M'
+BOT_TOKEN = '%telegram_bot_token%'
+DESTINATION = '%telegram_destination_id%'
 
-BOT_TOKEN = 'bot_token'
-DESTINATION = 'destination'
+
+URL = 'https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?openpage&cat=Work&sbcat=All&typ=Renewal'
+REQUEST_URL = '{url}&_={time}'
+SLOT_FORMAT = '%d %B %Y - %H:%M'
 
 logger = logging.getLogger('gnib_checker')
 hdlr = logging.FileHandler('/var/log/gnib_checker.log')
@@ -33,8 +35,11 @@ def main():
     while True:
 	time.sleep(5)
         dates_to_send = []
-        r = requests.get(REQUEST_URL.format(
-            url=URL, time=time.time()), verify=False)
+        try:
+	    r = requests.get(REQUEST_URL.format(
+                url=URL, time=time.time()), verify=False)
+        except Exception:
+            continue
         result = json.loads(r.content)
         if not result.get('slots'):
             logger.debug('No slots available')
@@ -52,10 +57,12 @@ def main():
         if dates_to_send:
             message = 'Available slots found!\n{slots}'.format(
                 slots='\n'.join(dates_to_send))
-            bot.send_message(DESTINATION, message)
-            logger.info(message)
+            try:
+                bot.send_message(DESTINATION, message)
+                logger.info(message)
+            except Exception:
+                continue
         sent_dates = dates_to_send
-
 
 if __name__ == "__main__":
     main()
